b166f2f4971e888f8e15eeecf6ece95c4c707233
steve-sr <gilberthsolisrios@gmail.com>
2026-01-24 13:58:24 -0600

    Enable SSL CA for Aiven

--- config.py
+++ config.py
@@ -1,9 +1,16 @@
 import os
-from dotenv import load_dotenv
+from pathlib import Path
 
-load_dotenv()
+BASE_DIR = Path(__file__).resolve().parent
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    CA_PATH = os.getenv("DB_SSL_CA", str(BASE_DIR / "certs" / "ca.pem"))
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {"ca": CA_PATH}
+        }
+    }
